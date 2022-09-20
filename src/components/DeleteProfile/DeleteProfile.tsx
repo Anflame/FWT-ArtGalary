d2a3923ca8ba8c7512eaf6ad948e81110864a028
Anflame <anflame@mail.ru>
2022-09-20 16:20:23 +0400

    done modal deleteProfile

---
+++ src/components/DeleteProfile/DeleteProfile.tsx
@@ -0,0 +1,56 @@
+import React, { Dispatch, FC, SetStateAction } from 'react';
+import cn from 'classnames/bind';
+import { Context } from '../../hooks/Context';
+import Button from '../Button';
+import { ReactComponent as CloseIcon } from '../../assets/images/closeIcon.svg';
+import { ReactComponent as DeleteIcon } from '../../assets/images/deleteProfileIcon.svg';
+import styles from './styles.module.scss';
+
+const cx = cn.bind(styles);
+
+type DeleteProfileProps = {
+  isShowDeleteProfile: boolean;
+  setIsShowDeleteProfile: Dispatch<SetStateAction<boolean>>;
+};
+
+export const DeleteProfile: FC<DeleteProfileProps> = ({
+  isShowDeleteProfile,
+  setIsShowDeleteProfile,
+}) => {
+  const { theme } = Context();
+  const handleDeleteProfile = () => {};
+  return (
+    <>
+      {isShowDeleteProfile && (
+        <section className={cx('deleteProfile')}>
+          <div className={cx('deleteProfileContent')}>
+            <DeleteIcon className={cx('deleteProfileIcon')} />
+            <h3 className={cx('deleteProfileHeading')}>
+              Do you want to delete this artist profile?
+            </h3>
+            <p className={cx('deleteProfileText')}>
+              You will not be able to recover this profile afterwards.
+            </p>
+            <Button
+              handleClick={handleDeleteProfile}
+              className={'logInSigUpBtn'}
+            >
+              delete
+            </Button>
+            <p
+              className={cx('deleteProfileCancel')}
+              onClick={() => setIsShowDeleteProfile(false)}
+            >
+              cancel
+            </p>
+            <CloseIcon
+              onClick={() => setIsShowDeleteProfile(false)}
+              fill={theme === 'dark' ? '#9C9C9C' : '#575757'}
+              className={cx('closeIcon')}
+            />
+          </div>
+        </section>
+      )}
+    </>
+  );
+};
---
+++ src/stories/DeleteProfile.stories.tsx
@@ -0,0 +1,18 @@
+import React, { useContext } from 'react';
+
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+import DeleteProfile from '../components/DeleteProfile';
+
+export default {
+  title: 'DeleteProfile',
+  component: DeleteProfile,
+} as ComponentMeta<typeof DeleteProfile>;
+
+const Template: ComponentStory<typeof DeleteProfile> = (args) => <DeleteProfile {...args} />;
+
+export const Primary = Template.bind({});
+
+Primary.args = {
+  isShowDeleteProfile: false,
+};
+
