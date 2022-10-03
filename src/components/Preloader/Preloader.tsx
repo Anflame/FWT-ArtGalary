c1539b81133d3892c96df44094a1a8710339c9b2
Anflame <anflame@mail.ru>
2022-10-03 16:33:36 +0400

    done preloder

--- src/components/Preloader/Preloader.tsx
+++ src/components/Preloader/Preloader.tsx
@@ -1,11 +1,15 @@
 import React, { FC } from 'react';
 import cn from 'classnames/bind';
+import { ReactComponent as PreloaderIcon } from '../../assets/images/preloader.svg';
 import styles from './styles.module.scss';
 
 const cx = cn.bind(styles);
 
 export const Preloader: FC = () => (
   <div className={cx('preloaderWrapp')}>
-    <div className={cx('loader')}></div>
+    <PreloaderIcon className={cx('preloader')} />
+    <PreloaderIcon className={cx('preloader')} />
+    <PreloaderIcon className={cx('preloader')} />
+    <PreloaderIcon className={cx('preloader')} />
   </div>
 );
