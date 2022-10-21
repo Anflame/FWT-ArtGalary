7f5ffd3fba982e0bd1e5e105557c8d575f3034c1
Anflame <anflame@mail.ru>
2022-10-21 16:09:16 +0400

    return dev styles

--- src/components/PainterArtworks/PainterArtworks.tsx
+++ src/components/PainterArtworks/PainterArtworks.tsx
@@ -20,7 +20,7 @@ export const PainterArtWorks: FC = () => {
     ({ painterProfile: { painterProfileInfo } }) => painterProfileInfo,
   );
   const [isShowEditPainting, setIsShowEditPainting] = useState(false);
-  const [isShowSlider, setIsShowSlider] = useState(true);
+  const [isShowSlider, setIsShowSlider] = useState(false);
   const [currentPage, setCurrentPage] = useState(1);
 
   return (
